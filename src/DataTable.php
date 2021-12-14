84e4c5f70ff4ec83b7e00e74941514e3ecdc12c9
Anton Mironov <MironovAB@gppc.ru>
2021-12-14 10:17:20 +0300

    initial

---
+++ src/DataTable.php
@@ -0,0 +1,250 @@
+<?php
+declare(strict_types = 1);
+
+namespace Blacky0892\LaravelDatatables;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+
+class DataTable
+{
+    private int     $draw;
+
+    private int     $start; // С какой записи выбирать
+
+    private int     $rowPerPage; // Количество записей на странице
+
+    private array   $columnNameArr; // список всех столбцов
+
+    private string  $columnName; // Имя столбца для сортировки
+
+    private string  $columnSortOrder;  // Порядок сортировки
+
+    private ?string $searchValue; // Значение в строке поиска
+
+    private Model   $model; // Модель для поиска
+
+    private array   $filters; // установленные фильтры
+
+    private bool    $isFilter = false; // Установлен ли хоть один фильтр
+
+    private Builder $records; // Запрос для получения записей из базы
+
+    private int     $totalRecords; // Общее количество записей в таблице
+
+    /**
+     * Инициализация
+     * @param Request $request Пришедшие данные из DataTable
+     * @param string  $model   Модель для поиска
+     */
+    public function __construct(Request $request, string $model)
+    {
+        $this->draw       = (int) $request->get('draw');
+        $this->start      = (int) $request->get('start');
+        $this->rowPerPage = (int) $request->get('length');
+
+        $orderArr    = $request->get('order');
+        $columnIndex = $orderArr[0]['column'];
+
+        $this->columnNameArr   = $request->get('columns');
+        $this->columnName      = $this->columnNameArr[$columnIndex]['data'];
+        $this->columnSortOrder = $orderArr[0]['dir'];
+        $this->searchValue     = $request->get('search')['value'];
+
+        $this->model        = new $model();
+        $this->totalRecords = $this->getTotalCount();
+
+        $this->records = $this->model::query();
+    }
+
+    /**
+     * Общее количество записей в таблице
+     * @return int
+     */
+    public function getTotalCount(): int
+    {
+        return $this->model::count();
+    }
+
+    /**
+     * Установка простых фильтров
+     * @param array $filters Список фильтров вида [name, fk, child],
+     *                       где fk - указатель, является ли столбец внешним ключом,
+     *                       prefix - префикс для поля
+     *                       child - промежуточная таблица
+     */
+    public function simpleFilters(array $filters)
+    {
+        $this->checkFilters(array_column($filters, 'name'));
+        foreach ($filters as $filter) {
+            $this->simpleFilter($filter['name'], $filter['fk'], $filter['prefix'] ?? null, $filter['child'] ?? null);
+        }
+    }
+
+    /**
+     * Проверка установлены ли определенные фильтры
+     * @param array $filters
+     */
+    private function checkFilters(array $filters)
+    {
+        foreach ($filters as $filter) {
+            $this->filters[$filter] = $this->columnFilter($filter);
+        }
+    }
+
+    /**
+     * Фильтрация по колонке
+     * @param string $needle Имя колонки для фильтрации
+     * @return string
+     */
+    private function columnFilter(string $needle): ?string
+    {
+        $key = array_search($needle, array_column($this->columnNameArr, 'data'));
+
+        return $this->columnNameArr[$key]['search']['value'];
+    }
+
+    /**
+     * Установка простого фильтра - поиск по значению
+     * @param  string  $filter  Столбец для фильтрации
+     * @param  bool  $isFK  Является ли столбец внешним ключом?
+     *                       Если да, добавляем к его названию _id
+     * @param  string|null  $prefix
+     * @param  string|null  $child Промежуточная таблица
+     */
+    private function simpleFilter(string $filter, bool $isFK = false, string $prefix = null, string $child = null)
+    {
+        $f = $this->filters[$filter];
+        if (!is_null($f) && intval($f) !== 0) {
+            $where         = $isFK ? $filter . '_id' : $filter;
+            $where = is_null($prefix) ? $where : $prefix . $where;
+            if(is_null($child))
+            {
+                $this->records = $this->records->where($where, $f);
+            }
+            else {
+                $this->records = $this->records->whereHas($child, function($q) use ($where, $f){
+                    $q->where($where, $f);
+                });
+            }
+            $this->isFilter = true;
+        }
+    }
+
+    /**
+     * Установка фильтра по дате
+     * @param string $field Имя поля с датой
+     */
+    public function dateFilter(string $field = 'date')
+    {
+        $this->checkFilters([$field]);
+        if (!is_null($this->filters[$field]) && $this->filters[$field] !== 'null') {
+            $dates     = explode(' — ', $this->filters[$field]);
+            $startDate = Carbon::parse($dates[0]);
+            $end       = $dates[1] ?? $dates[0];
+            $endDate   = Carbon::parse($end)->setTime(23, 59, 59);
+
+            $this->records = $this->records->whereBetween($field, [$startDate, $endDate]);
+            $this->isFilter = true;;
+        }
+    }
+
+    /**
+     * Текстовый поиск в указанных колонках
+     * @param array $columns Список колонок для поиска. Может быть именем колонки или массивом из двух элементов:
+     *                       key - колонка для поиска, value - значение для поиска
+     */
+    public function searchFilter(array $columns)
+    {
+        if ($this->searchValue) {
+            // Весь поиск из строки в отдельном Where, т.к тут есть ИЛИ
+            $this->records = $this->records->where(function ($q) use ($columns) {
+                foreach ($columns as $column) {
+                    if (is_array($column)) {
+                        $key   = $column['key'];
+                        $value = $column['value'];
+                    } else {
+                        $key   = $column;
+                        $value = $this->searchValue;
+                    }
+                    /* Для поиска в том случае если разные таблицы
+                    имеют одинаковые ключи указываем имя таблицы */
+                    $key = $this->model->getTable() . '.' . $key;
+
+                    $q->orWhere($key, 'like', '%' . $value . '%');
+                }
+            });
+            $this->isFilter = true;
+        }
+    }
+
+    /**
+     * Сортировка значений, находящихся в другой таблице
+     * @param  string  $table  Таблица со значением
+     * @param  string  $field  Поле связывающее с внешней таблицой
+     * @param  string  $order  Поле для сортировки
+     * @param  bool|null  $isFK Является ли данная таблица внешней
+     */
+    public function sortWithJoin(string $table, string $field, string $order, bool $isFK = true)
+    {
+        if($isFK){
+            $this->records = $this->records
+                ->join($table, $table . '.id', '=', $this->model->getTable() . '.' . $field);
+        }
+        else{
+            $this->records = $this->records
+                ->join($table, $table . '.'. $field, '=', $this->model->getTable() . '.id');
+        }
+        $this->records =
+            $this->records
+                ->orderBy($table . '.' . $order, $this->columnSortOrder)
+                // Хак для избавления от выбора полей с одинаковым именем из разных таблиц
+                ->select($this->model->getTable() . '.*', $table . '.' . $order . ' as orderField');
+    }
+
+    /**
+     *  Сортировка по указанному полю
+     */
+    public function simpleSort()
+    {
+        $this->records = $this->records->orderBy($this->columnName, $this->columnSortOrder);
+    }
+
+    /**
+     * Получение массива для возврата в DataTable
+     * @param array $data Массив с данными
+     * @return array
+     */
+    public function getDTResponse(array $data): array
+    {
+        return [
+            'draw'                 => $this->draw,
+            'iTotalRecords'        => $this->totalRecords,
+            'iTotalDisplayRecords' => $this->getTotalRecordsWithFilter(),
+            'aaData'               => $data,
+        ];
+    }
+
+    /**
+     * Общее количество отфильтрованных записей
+     * @return int
+     */
+    public function getTotalRecordsWithFilter(): int
+    {
+        return $this->isFilter ? $this->records->count() : $this->totalRecords;
+    }
+
+    /**
+     * Получение всех записей с фильтрами
+     * @return Collection
+     */
+    public function getRecords(): Collection
+    {
+        return $this->records->skip($this->start)
+            ->take($this->rowPerPage)
+            ->get();
+    }
+}
---
+++ src/LaravelDatatableServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Blacky0892\LaravelDatatables;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelDatatableServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
